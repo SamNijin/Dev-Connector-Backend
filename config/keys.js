56ba24d79ec80ad60c179fe4233ed84ea0ae650a
SAM NIJIN <samnijin562@gmail.com>
2025-01-30 09:20:54 +0530

    DB name added

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 
 module.exports = {
-  mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterdevconnector.thjgj.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDevConnector`,
+  mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterdevconnector.thjgj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=ClusterDevConnector`,
 };
